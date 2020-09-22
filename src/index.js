a7536c0fb80d5d07b62b9c605df76940dc99746e
Sean Blessing <snblessing@gmail.com>
2020-09-22 17:58:43 -0400

    completing the game - lifting state up

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  
   render() {
     return (
-      <button className="square">
+      <button 
+        className="square" 
+        onClick={() => this.props.onClick()}
+      >
         {this.props.value}
       </button>
     );
@@ -13,8 +17,18 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (<Square 
+              value={this.state.squares[i]} 
+              onClick={() => this.handleClick(i)}
+            />
+    );
   }
 
   render() {
